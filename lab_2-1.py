8afe7b59c5c0b6ca4e489e8fc070b29e64692909
P22inolan <p22inolan@fairfieldprep.org>
2021-10-25 09:53:14 -0400

    Submit 2-1

---
+++ lab_2-1.py
@@ -0,0 +1,12 @@
+# Author: IBN (AMDG) 10/25/2021
+import math
+print(math.ceil(7.15))
+# Answer: 8
+print(math.floor(19.156))
+# Answer: 19
+print(math.fabs(-42))
+# Answer: 42
+print(math.pow(2, 3))
+# Answer: 8
+print(math.log10(100))
+# Answer: 2
