52e63edebfe3f56fc85ad7f7783c71500a3cafa8
P22inolan <p22inolan@fairfieldprep.org>
2021-10-26 10:13:15 -0400

    Submit 4-1

---
+++ lab_4-1.py
@@ -0,0 +1,12 @@
+# Author: IBN (AMDG) 10/26/2021
+
+print('Fairfield' + ' Prep')
+
+print("-" * 20)
+
+print('I\'m loving this short story I\'m reading, "The Fall of the House of Usher"')
+
+print(len('I\'m loving this short story I\'m reading, "The Fall of the House of Usher"'))
+
+character = "apple"[3]
+print(character)
